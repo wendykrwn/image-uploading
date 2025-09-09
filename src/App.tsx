2fb3bbffed2ff1da290f65edbeba6d47826371f0
wendykrwn <wendykrwn@gmail.com>
2025-09-09 21:06:41 +0200

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import ImageUploader from './components/image-uploader'
+import Navbar from './components/navbar'
 
 function App() {
 
   return (
-    <>
+    <div className='main'>
+      <Navbar/>
       <ImageUploader/>
-    </>
+    </div>
   )
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,22 @@
+import logo from "/logo.svg"
+import moon from "/src/assets/Moon_fill.svg"
+import sun from "/src/assets/Sun_fill.svg"
+
 const Navbar = () => {
     return (
         <div className="navbar">
-            imageUpload
+            <div className="navbar-content">
+                <a href="#">
+                    <img src={logo} alt="logo"/>
+                </a>
+                <button className="toggle-theme"
+                    type="button"
+                    data-theme-toggle
+                    aria-label="Change to dark theme"
+                >
+                    <img src={moon} alt="image de lune"/>
+                </button>
+            </div>
         </div>
     )
 }
