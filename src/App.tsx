855ca9412720f9beacec5e58bb34a893c5ded416
wendykrwn <wendykrwn@gmail.com>
2025-09-18 17:16:35 +0200

    reinitialise l'app lorsqu'on appuie sur le logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 import ImageUploader from './components/imageUploader'
 import Navbar from './components/navbar'
 
 function App() {
+  const [resetKey, setResetKey] = useState(0)
 
+  const handleReset = () => {
+    setResetKey(prev => prev + 1)
+  }
   return (
     <div className='app'>
-      <Navbar/>
-      <ImageUploader/>
+      <Navbar onLogoClick={handleReset}/>
+      <ImageUploader key={resetKey}/>
     </div>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,39 @@
+import { useEffect, useState } from "react";
 import logo from "/logo.svg"
 import moon from "/src/assets/Moon_fill.svg"
 import sun from "/src/assets/Sun_fill.svg"
 
-const Navbar = () => {
+type NavbarProps = {
+    onLogoClick: () => void
+  }
+
+const Navbar = ({ onLogoClick }: NavbarProps) => {
+    const [theme, setTheme] = useState<"light" | "dark">("light");
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme);
+      }, [theme]);
+    const toggleTheme = () => {
+    setTheme(prev => (prev === "light" ? "dark" : "light"));
+    };
+
     return (
         <div className="navbar">
             <div className="navbar-content">
-                <a href="#">
+                <div className="pointer" onClick={onLogoClick} >
                     <img src={logo} alt="logo"/>
-                </a>
+                </div>
                 <button className="toggle-theme"
                     type="button"
                     data-theme-toggle
                     aria-label="Change to dark theme"
+                    onClick={toggleTheme}
                 >
-                    <img src={moon} alt="image de lune"/>
+                    {
+                        theme=="light" ? 
+                        <img src={moon} alt="image de lune"/>
+                        :
+                        <img src={sun} alt="image de soleil"/>
+                    }
                 </button>
             </div>
         </div>
